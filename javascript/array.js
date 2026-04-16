7d3ba20a8acbc9e8c2daf0e95a6e687e83c183fa
Daksh <d8550968@gmail.com>
2026-04-16 12:16:30 +0530

    additonal knowledge

--- javascript/array.js
+++ javascript/array.js
@@ -7,6 +7,7 @@ console.log(person);
 console.log(person.name);
 console.log(person.age);
 console.log(person.city);
+console.log(typeof(person));
 
 //array data type
 
@@ -32,6 +33,28 @@ let squaredarray = num.map(num => num*num);
 console.log(squaredarray);
 console.log(num);
 
-//filter on array
+//funciton with now parameters
+function greet(){
+    console.log("Hello,World!")
+}
+greet();
+
+//normal function
+function multiply(a,b){
+    return a*b;
+}
+console.log(multiply(2,1.5));
+
+//function expression
+//arrow function
+const add = (a,b) => a+b;
+console.log(add(2,3));
+
+const divide = (a,b) => a/b;
+console.log(divide(10,3));
+
+//arrow funciton with single parameter
+let square = x => x*x;
+console.log(square(4));
 
 
